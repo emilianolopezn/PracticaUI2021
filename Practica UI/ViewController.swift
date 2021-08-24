c3c8fd7f449e956f830849ba4233303e1f06674a
Ulsa <ulsa@ulsa.com>
2021-08-24 16:14:14 -0700

    Initial Commit

---
+++ Practica UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica UI
+//
+//  Created by MaestroD1 on 8/24/21.
+//  Copyright © 2021 ULSA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
